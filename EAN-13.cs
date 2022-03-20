394180990779893c13569c4315fd0548c6c5aa7b
nevazhnoyauchus <86868064+nevazhnoyauchus@users.noreply.github.com>
2022-03-20 23:49:27 +0300

    Add files via upload

---
+++ EAN-13.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarCodeGenerator
+{
+    class EAN_13
+    {
+        /// <summary>
+        /// Получает цифру контрольной суммы для расчета G строки кодировки
+        /// </summary>
+        /// <param name="contorlSummNumber"></param>
+        /// <returns>G строку кодировки</returns>
+        public static string GetG(int contorlSummNumber)
+        {
+            switch (contorlSummNumber)
+            {
+                case '0': return "LLLLLL";
+                case '1': return "LLGLGG";
+                case '2': return "LLGGLG";
+                case '3': return "LLGGGL";
+                case '4': return "LGLLGG";
+                case '5': return "LGGLLG";
+                case '6': return "LGGGLL";
+                case '7': return "LGLGLG";
+                case '8': return "LGLGGL";
+                case '9': return "LGGLGL";
+                default: return "LLLLLL";
+            }
+        }
+
+        /// <summary>
+        /// Расчитывает код цифры исходя из G или L позиции в коде
+        /// </summary>
+        /// <param name="LorGCode"></param>
+        /// <param name="number"></param>
+        /// <returns>Строку кодировки цифры</returns>
+        public static string GetLeftCode(char LorGCode, char number)
+        {
+            switch (number)
+            {
+                case 'L': return GetLeftL(number);
+                case 'G': return GetLeftG(number);
+                default: return "0100111";
+            }
+        }
+
+        /// <summary>
+        /// Расчитывает код полученной цифры по G кодировке
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>Строка кода цифры в G кодировке</returns>
+        public static string GetLeftG(char number)
+        {
+            switch (number)
+            {
+                case '0': return "0100111";
+                case '1': return "0110011";
+                case '2': return "0011011";
+                case '3': return "0100001";
+                case '4': return "0011101";
+                case '5': return "0111001";
+                case '6': return "0000101";
+                case '7': return "0010001";
+                case '8': return "0001001";
+                case '9': return "0010111";
+                default: return "0100111";
+            }
+        }
+
+        /// <summary>
+        /// Расчитывает код полученной цифры по L кодировке
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>Строка кода цифры в L кодировке</returns>
+        public static string GetLeftL(char number)
+        {
+            switch (number)
+            {
+                case '0': return "0001101";
+                case '1': return "0011001";
+                case '2': return "0010011";
+                case '3': return "0111101";
+                case '4': return "0100011";
+                case '5': return "0110001";
+                case '6': return "0101111";
+                case '7': return "0111011";
+                case '8': return "0110111";
+                case '9': return "0001011";
+                default: return "0001101";
+            }
+        }
+
+        /// <summary>
+        /// Расчитывает код полученной цифры по R кодировке
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns>Строка кода цифры в L кодировке</returns>
+        public static string GetRight(char s)
+        {
+            switch (s)
+            {
+                case '0': return "1110010";
+                case '1': return "1100110";
+                case '2': return "1101100";
+                case '3': return "1000010";
+                case '4': return "1011100";
+                case '5': return "1001110";
+                case '6': return "1010000";
+                case '7': return "1000100";
+                case '8': return "1001000";
+                case '9': return "1110100";
+                default: return "0001101";
+            }
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BarCodeGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            SPCode.Children.Clear();
+            string code = Generator();
+            //MessageBox.Show(code);
+
+            string summ = (((int)code[0] + (int)code[2] + (int)code[4] + (int)code[6] + (int)code[8] + (int)code[10]) * 3 + (int)code[1] + (int)code[3] + (int)code[5] + (int)code[7] + (int)code[9] + (int)code[11]).ToString();            
+            char controlSumm = (summ[summ.Length - 1]);
+
+            TextBlock TbControlSumm = new TextBlock()
+            {
+                Text = "" + controlSumm,
+                FontSize = 16,
+                TextAlignment = TextAlignment.Center,
+                Margin = new Thickness(0, 100, 1, 0)
+            };
+            SPCode.Children.Add(TbControlSumm);
+
+            string binCode = "101";
+            for (int i = 0; i < 3; i++)
+            {
+                SPCode.Children.Add(Draw(binCode[i], true));
+            }
+
+            StackPanel SPLeft = new StackPanel() 
+            {
+                Orientation = Orientation.Vertical,
+                Height = 150
+            };
+            StackPanel SPLeftCode = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal,
+            };
+            SPCode.Children.Add(SPLeft);
+            SPLeft.Children.Add(SPLeftCode);
+            string gString = EAN_13.GetG(controlSumm);
+            TextBlock TbLeftNumber = new TextBlock()
+            {
+                Text = "",
+                FontSize = 16,
+                TextAlignment = TextAlignment.Center,
+            };
+            SPLeft.Children.Add(TbLeftNumber);
+
+            for (int i = 0; i < 6; i++)
+            {
+                string codeTemp = "";
+                if (gString[i] == 'L')
+                {
+                    codeTemp = EAN_13.GetLeftG(code[i]);
+                }
+                if (gString[i] == 'G')
+                {
+                    codeTemp = EAN_13.GetLeftL(code[i]);
+                }
+                for(int j = 0; j < codeTemp.Length; j++)
+                {
+                    SPLeftCode.Children.Add(Draw(codeTemp[j], false));
+                }
+                TbLeftNumber.Text += " " + code[i];
+            }
+
+
+
+            binCode = "01010";
+            for (int i = 0; i < 5; i++)
+            {
+                SPCode.Children.Add(Draw(binCode[i], true));
+            }
+
+
+
+            StackPanel SPRight = new StackPanel()
+            {
+                Orientation = Orientation.Vertical,
+                Height = 150
+            };
+            SPCode.Children.Add(SPRight);
+
+            StackPanel SPRightCode = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal,
+            };
+            SPRight.Children.Add(SPRightCode);
+
+            TextBlock TbRightNumber = new TextBlock()
+            {
+                Text = "",
+                FontSize = 16,
+                TextAlignment = TextAlignment.Center,
+            };
+            SPRight.Children.Add(TbRightNumber);
+
+            for (int i = 6; i < code.Length; i++)
+            {
+                string codeTemp = "";
+                codeTemp = EAN_13.GetRight(code[i]);
+                for (int j = 0; j < codeTemp.Length; j++)
+                {
+                    SPRightCode.Children.Add(Draw(codeTemp[j], false));
+                }
+                TbRightNumber.Text += " " + code[i];
+            }
+
+            binCode = "101";
+            for (int i = 0; i < 3; i++)
+            {
+                SPCode.Children.Add(Draw(binCode[i], true));
+            }
+        }
+
+        public static Rectangle Draw(char number, bool IsShield)
+        {
+            if (number == '1')
+            {
+                if(IsShield == true)
+                {
+                    Rectangle rec = new Rectangle()
+                    {
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 2,
+                        SnapsToDevicePixels = true,
+                        Height = 120,
+                        VerticalAlignment = VerticalAlignment.Top
+                    };
+                    return rec;
+                }
+                else
+                {
+                    Rectangle rec = new Rectangle()
+                    {
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 2,
+                        SnapsToDevicePixels = true,
+                        Height = 100
+                    };
+                    return rec;
+                }            
+            }
+            else
+            {
+                if (IsShield == true)
+                {
+                    Rectangle rec = new Rectangle()
+                    {
+                        Stroke = Brushes.White,
+                        StrokeThickness = 2,
+                        SnapsToDevicePixels = true,
+                        Height = 120,
+                        VerticalAlignment = VerticalAlignment.Top
+                    };
+                    return rec;
+                }
+                else
+                {
+                    Rectangle rec = new Rectangle()
+                    {
+                        Stroke = Brushes.White,
+                        StrokeThickness = 2,
+                        SnapsToDevicePixels = true,
+                        Height = 100
+                    };
+                    return rec;
+                }
+            }
+        }
+        public static string Generator()
+        {
+            Random randCode = new Random();
+            string code = "";
+
+            int dayTemp = randCode.Next(31);
+            if (dayTemp < 10)
+            {
+                code += "0" + dayTemp.ToString();
+            }
+            else
+            {
+                code += dayTemp.ToString();
+            }
+
+            int mounthTemp = randCode.Next(12);
+            if (mounthTemp < 10)
+            {
+                code += "0" + mounthTemp.ToString();
+            }
+            else
+            {
+                code += mounthTemp.ToString();
+            }
+
+            int yearTemp = randCode.Next(1, 22);
+            if (yearTemp < 10)
+            {
+                code += "0" + yearTemp.ToString();
+            }
+            else
+            {
+                code += yearTemp.ToString();
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                int numberTemp = randCode.Next(9);
+                code += numberTemp.ToString();
+            }
+            return code;
+        }
+
+    }
+}
